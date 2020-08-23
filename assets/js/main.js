073e04b9489148479f57f1e7b5a673664b177459
aljuned <=>
2020-08-23 13:09:43 +0600

    Upload jQuary Plaugin from Al Juned PC

---
+++ assets/js/main.js
@@ -0,0 +1,465 @@
+(function($) {
+    "use strict";
+
+    /* Preloader */
+    $(window).load(function() {
+        $("#loading").fadeOut(1000)
+    })
+
+    // One Page Nav
+    var top_offset = $('.header-area').height() - 10;
+    $('.main-menu nav ul').onePageNav({
+        currentClass: 'active',
+        scrollOffset: top_offset,
+    });
+
+
+    $(window).on('scroll', function() {
+        var scroll = $(window).scrollTop();
+        if (scroll < 245) {
+            $(".header-sticky").removeClass("sticky");
+        } else {
+            $(".header-sticky").addClass("sticky");
+        }
+    });
+
+
+
+    // mainSlider
+    function mainSlider() {
+        var BasicSlider = $('.slider-active');
+        BasicSlider.on('init', function(e, slick) {
+            var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+            doAnimations($firstAnimatingElements);
+        });
+        BasicSlider.on('beforeChange', function(e, slick, currentSlide, nextSlide) {
+            var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+            doAnimations($animatingElements);
+        });
+        BasicSlider.slick({
+            autoplay: false,
+            autoplaySpeed: 10000,
+            dots: false,
+            fade: true,
+            arrows: false,
+            responsive: [
+                { breakpoint: 767, settings: { dots: false, arrows: false } }
+            ]
+        });
+
+        function doAnimations(elements) {
+            var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            elements.each(function() {
+                var $this = $(this);
+                var $animationDelay = $this.data('delay');
+                var $animationType = 'animated ' + $this.data('animation');
+                $this.css({
+                    'animation-delay': $animationDelay,
+                    '-webkit-animation-delay': $animationDelay
+                });
+                $this.addClass($animationType).one(animationEndEvents, function() {
+                    $this.removeClass($animationType);
+                });
+            });
+        }
+    }
+    mainSlider();
+
+
+    // owlCarousel
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            767: {
+                items: 3
+            },
+            992: {
+                items: 5
+            }
+        }
+    })
+
+
+    /* magnificPopup img view */
+    $('.popup-image').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+    /* magnificPopup video view */
+    $('.popup-video').magnificPopup({
+        type: 'iframe'
+    });
+
+
+    // isotop
+    $('.grid').imagesLoaded(function() {
+        // init Isotope
+        var $grid = $('.grid').isotope({
+            itemSelector: '.grid-item',
+            percentPosition: true,
+            masonry: {
+                // use outer width of grid-sizer for columnWidth
+                columnWidth: '.grid-item',
+            }
+        });
+    });
+
+    // filter items on button click
+    $('.portfolio-menu').on('click', 'button', function() {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+
+    //for menu active class
+    $('.portfolio-menu button').on('click', function(event) {
+        $(this).siblings('.active').removeClass('active');
+        $(this).addClass('active');
+        event.preventDefault();
+    });
+
+    // scrollToTop
+    $.scrollUp({
+        scrollName: 'scrollUp',
+        topDistance: '300',
+        scrollText: '<i class="fa fa-angle-up"></i>', // 
+        activeOverlay: false,
+
+    });
+
+    // WOW active
+    new WOW().init();
+
+    /* Wavify Plugin */
+    $('#wave').wavify({
+        height: 60,
+        bones: 2,
+        amplitude: 40,
+        color: '#0bd',
+        speed: .25
+    });
+
+    /* Firefly plugins */
+
+    $.firefly({
+        on: '#firefly',
+        minPixel: 3,
+        maxPixel: 10,
+        color: '#fff',
+        total: 65,
+
+    });
+
+    /* nice seclection */
+    $('select').niceSelect();
+
+    /* Elevate zoom */
+
+    $('#zoom_01').elevateZoom();
+
+    /* Time Countdown */
+    $('[data-countdown]').each(function() {
+        var $this = $(this),
+            finalDate = $(this).data('countdown');
+        $this.countdown(finalDate, function(event) {
+            $this.html(event.strftime('%D days %H:%M:%S'));
+        });
+    });
+
+    /* bg Video */
+
+    jQuery(".bg-video").YTPlayer({
+        showControls: false,
+    });
+
+    /* Scrolll opacity */
+
+    var animation_start_pos = 0,
+        animation_end_pos = 1000; //where you want the animation to stop
+
+    $(document).scroll(function() {
+        var scroll_pos = $(this).scrollTop();
+        var percentScrolled = parseFloat(scroll_pos / animation_end_pos);
+        jQuery('.people-opacity').css('opacity', percentScrolled);
+    });
+
+    /* Google map */
+
+    function basicmap() {
+        // Basic options for a simple Google Map
+        // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+        var mapOptions = {
+            // How zoomed in you want the map to start at (always required)
+            zoom: 11,
+            scrollwheel: false,
+            // The latitude and longitude to center the map (always required)
+            center: new google.maps.LatLng(24.903561, 91.873611), // New York
+            // This is where you would paste any style found on Snazzy Maps.
+            styles: [{
+                    "featureType": "all",
+                    "elementType": "all",
+                    "stylers": [{
+                            "invert_lightness": true
+                        },
+                        {
+                            "saturation": "-9"
+                        },
+                        {
+                            "lightness": "0"
+                        },
+                        {
+                            "visibility": "simplified"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "landscape.man_made",
+                    "elementType": "all",
+                    "stylers": [{
+                        "weight": "1.00"
+                    }]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "all",
+                    "stylers": [{
+                        "weight": "0.49"
+                    }]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "labels",
+                    "stylers": [{
+                            "visibility": "on"
+                        },
+                        {
+                            "weight": "0.01"
+                        },
+                        {
+                            "lightness": "-7"
+                        },
+                        {
+                            "saturation": "-35"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "labels.text",
+                    "stylers": [{
+                        "visibility": "on"
+                    }]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "labels.text.stroke",
+                    "stylers": [{
+                        "visibility": "off"
+                    }]
+                },
+                {
+                    "featureType": "road.highway",
+                    "elementType": "labels.icon",
+                    "stylers": [{
+                        "visibility": "on"
+                    }]
+                }
+            ]
+        };
+        // Get the HTML DOM element that will contain your map 
+        // We are using a div with id="map" seen below in the <body>
+        var mapElement = document.getElementById('contact-map');
+
+        // Create the Google Map using our element and options defined above
+        var map = new google.maps.Map(mapElement, mapOptions);
+
+        // Let's also add a marker while we're at it
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(24.903561, 91.873611),
+            map: map,
+            title: 'Cryptox'
+        });
+    }
+    if ($('#contact-map').length != 0) {
+        google.maps.event.addDomListener(window, 'load', basicmap);
+    }
+
+    /* price ranege */
+
+
+    $("#slider-range").slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [75, 300],
+        slide: function(event, ui) {
+            $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+        }
+    });
+    $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+        " - $" + $("#slider-range").slider("values", 1));
+
+    /* instragram feed */
+
+    var feed = new Instafeed({
+        get: 'user',
+        userId: 6665768655,
+        accessToken: '6665768655.1677ed0.313e6c96807c45d8900b4f680650dee5',
+        target: 'Instafeed',
+        resolution: 'thumbnail',
+        limit: 6,
+        template: '<li><a href="{{link}}" target="_new"><img src="{{image}}" /></a></li>',
+    });
+    feed.run();
+
+
+    /* ---- particles.js config ---- */
+
+    particlesJS("particles-js", {
+        "particles": {
+            "number": {
+                "value": 380,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#ffffff"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 0.5,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 140,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true
+    });
+
+
+    /* ---- stats.js config ---- */
+
+    var count_particles, stats, update;
+    stats = new Stats;
+    stats.setMode(0);
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '0px';
+    document.body.appendChild(stats.domElement);
+    count_particles = document.querySelector('.js-count-particles');
+    update = function() {
+        stats.begin();
+        stats.end();
+        if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+            count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+        }
+        requestAnimationFrame(update);
+    };
+    requestAnimationFrame(update);
+
+
+
+
+
+
+})(jQuery);
